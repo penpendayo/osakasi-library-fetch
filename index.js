66d834c1b3038470e2a7d0cc9ed011021487b0b4
penpen <yamaa.exception@gmail.com>
2021-05-23 13:28:28 +0900

    tablesorterを使ってソートできるようにした close #3

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ const main = async () => {
       const document = dom.window.document;
       const element = document.querySelector(".opac_data_list_ex");
       let table = document.createElement("table");
+      table.classList.add("itirann", "tablesorter-blue");
       let thead = document.createElement("thead");
       let tbody = document.createElement("tbody");
       Array.from(element.children[1].children).forEach((TR, row) => {
@@ -184,6 +185,14 @@ const main = async () => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/css/theme.blue.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js" defer></script>
+    <script >
+    window.addEventListener( 'DOMContentLoaded', function(){
+      $(".itirann").tablesorter();
+    })
+    </script>
 </head>
 <body>`;
   const htmlfooter = `</body>
